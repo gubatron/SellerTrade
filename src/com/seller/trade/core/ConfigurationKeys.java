8d65c2ed5dbc9b238609b03d6cfa0bd51282101c
gubatron <gubatron@gmail.com>
2015-01-11 01:34:22 -0500

    configuration keys cleanup

--- src/com/seller/trade/core/ConfigurationKeys.java
+++ src/com/seller/trade/core/ConfigurationKeys.java
@@ -31,38 +31,15 @@ public class ConfigurationKeys {
     public static final String ST_SERVER_PORT = "st.server_port";
     public static final String ST_SITE_NAME = "st.site_name";
     public static final String ST_TEMPLATES_FOLDER = "st.templates.folder";
-    public static final String ST_SITE_HTTP_PREFIX = "st.site_http_prefix";
     public static final String ST_SITE_SLOGAN = "st.site_slogan";
     public static final String ST_SITE_DESCRIPTION = "st.site_description";
     public static final String ST_SITE_KEYWORDS = "st.site_keywords";
-    public static final String ST_SEARCH_HASHTAGS = "st.search_hashtags";
-    public static final String ST_SITE_USER_ID = "st.site_user_id";
-    public static final String ST_JSON_PRETTY_PRINT = "st.json_pretty_print";
+    public static final String ST_SHIPPING_FROM = "st.shipping_from";
     public static final String ST_SERVER_IP = "st.server_ip";
-    public static final String ST_LIB_PATH = "st.lib_path";
     public static final String ST_IS_LOBBY_SERVER = "st.is_lobby_server";
     public static final String ST_USE_LAN_MAPPINGS= "st.use_lan_mappings";
-
-    public static final String ST_EMAIL_USERNAME = "st.emails.username";
-    public static final String ST_EMAIL_PASSWORD = "st.emails.password";
-    public static final String ST_EMAIL_SMTP_HOST = "st.emails.smtp.host";
-    public static final String ST_EMAIL_SMTP_PORT = "st.emails.smtp.port";
-    public static final String ST_EMAIL_BCC = "st.emails.bcc";
-    public static final String ST_EMAIL_FROM = "st.emails.from";
-    public static final String ST_EMAIL_RESET_RETRY_TIMEOUT = "st.emails.reset_retry_timeout";
-    public static final String ST_EMAIL_TEST = "st.emails.test"; 
-    public static final String ST_MAILING_LIST_SENDER_SLEEPING_TIME = "st.emails.mailing_list_sender_sleeping_time";
     public static final String ST_SEND_ACCESS_CONTROL_ALLOW_ORIGIN = "st.send_access_control_allow_origin";
     public static final String ST_SEARCH_HOPS = "st.search.hops";
-
-    public static final String AWS_ACCESS="aws.access";
-    public static final String AWS_SECRET="aws.secret";
-    public static final String AWS_SQS_SERVICE_URL = "aws.sqs.service_url";
-    public static final String AWS_SQS_WEEKLY_DIGEST_URL = "aws.sqs.weekly_digest_url";
-    public static final String AWS_SQS_NEWSLETTER_URL = "aws.sqs.newsletter_url";
-    
-    public static final String WEB_PATH = "website.path";
-
 
     protected ConfigurationKeys() {
     }
