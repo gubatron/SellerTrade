d4031df17a1dc882ed6d10c32ea0eab72c2e556e
gubatron <gubatron@gmail.com>
2015-04-02 13:53:09 -0400

    Fixed memory issue in linux

--- src/com/seller/trade/services/dht/DHTServiceImpl.java
+++ src/com/seller/trade/services/dht/DHTServiceImpl.java
@@ -130,8 +130,16 @@ public class DHTServiceImpl implements DHTService {
         }
     }
 
-    private static String toHex(byte[] arr) {
-        return LibTorrent.toHex(arr);
+    // copied from internet
+    final protected static char[] hexArray = "0123456789abcdef".toCharArray();
+    public static String toHex(byte[] bytes) {
+	char[] hexChars = new char[bytes.length * 2];
+	for ( int j = 0; j < bytes.length; j++ ) {
+	    int v = bytes[j] & 0xFF;
+	    hexChars[j * 2] = hexArray[v >>> 4];
+	    hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	}
+	return new String(hexChars);
     }
 
     private static byte[] sha1(String str) {
